ea77f90920f05a344e5d6dc6a945fcde5bb5b1d9
Karol Klimonczyk <k.klimonczyk@gmail.com>
2019-03-23 21:13:55 +0100

    Upload audio file

--- src/main/java/com/hackcumple/eventsapp/resources/SpeechResource.java
+++ src/main/java/com/hackcumple/eventsapp/resources/SpeechResource.java
@@ -3,10 +3,8 @@ package com.hackcumple.eventsapp.resources;
 import com.hackcumple.eventsapp.services.SpeechService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/speech")
@@ -15,14 +13,14 @@ public class SpeechResource {
 
     private final SpeechService speechService;
 
-    @GetMapping(path = "uploadAudio/{fileName}")
-    public ResponseEntity uploadAudio(@PathVariable("fileName")String fileName) {
-        return ResponseEntity.ok().body(speechService.uploadAudio(fileName));
-    }
-
     @GetMapping(path = "getTranscriptionDetails/{orderId}")
-    public ResponseEntity getTranscriptionDetails(@PathVariable("orderId")String orderId) {
+    public ResponseEntity getTranscriptionDetails(@PathVariable("orderId") String orderId) {
         speechService.GetTranscriptionDetails(orderId);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity uploadAudio(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok(this.speechService.uploadAudio(file));
     }
 }
--- src/main/java/com/hackcumple/eventsapp/services/SpeechService.java
+++ src/main/java/com/hackcumple/eventsapp/services/SpeechService.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.File;
 import java.util.Map;
 import java.util.Objects;
 
@@ -27,28 +29,28 @@ public class SpeechService {
     @Value("${listenbycode.api.url.download}")
     private String downloadUrl;
 
-    public String uploadAudio(String fileName) {
+    public String uploadAudio(MultipartFile file) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-        Resource resource = new ClassPathResource(fileName);
-        MultiValueMap<String, Object> map= new LinkedMultiValueMap<>();
+        Resource resource = new ClassPathResource((new File(file.getOriginalFilename())).getPath());
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
         map.add("appkey", apiKey);
         map.add("language", "pl-PL");
         map.add("file", resource);
         HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(map, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity response = restTemplate.postForEntity( uploadUrl, request, Map.class );
+        ResponseEntity response = restTemplate.postForEntity(uploadUrl, request, Map.class);
 
-        return ((Map)((Map) Objects.requireNonNull(response.getBody())).get("transcriptionResult")).get("orderId").toString();
+        return ((Map) ((Map) Objects.requireNonNull(response.getBody())).get("transcriptionResult")).get("orderId").toString();
     }
 
     public void GetTranscriptionDetails(String idOrder) {
         TranscriptionResult transcription = GetTranscription("idOrder");
     }
 
-    private TranscriptionResult GetTranscription(String idOrder){
+    private TranscriptionResult GetTranscription(String idOrder) {
         HttpHeaders headers = new HttpHeaders();
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(downloadUrl)
                 .queryParam("appkey", apiKey)
@@ -61,5 +63,4 @@ public class SpeechService {
 
         return response.getBody().getResult();
     }
-
 }
