db67e394ec9dabcdc309d4625407e80f2a0fb52b
pigeon <pigeon@localhost.localdomain>
2019-03-23 17:15:40 +0100

    some axios service concept

---
+++ ing-event-client/src/common/api.service.js
@@ -0,0 +1,72 @@
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { API_URL } from "@/common/config";
+
+const ApiService = {
+  init() {
+    Vue.use(VueAxios, axios);
+    Vue.axios.defaults.baseURL = API_URL;
+  },
+
+  query(resource, params) {
+    return Vue.axios.get(resource, params).catch(error => {
+      throw new Error(`[RWV] ApiService ${error}`);
+    });
+  },
+
+  get(resource, slug = "") {
+    return Vue.axios.get(`${resource}/${slug}`).catch(error => {
+      throw new Error(`[RWV] ApiService ${error}`);
+    });
+  },
+
+  post(resource, params) {
+    return Vue.axios.post(`${resource}`, params);
+  },
+
+};
+
+export default ApiService;
+
+
+export const TalkService = {
+  query(type, params) {
+    return ApiService.query("articles" + (type === "feed" ? "/feed" : ""), {
+      params: params
+    });
+  },
+  get(slug) {
+    return ApiService.get("talk", slug);
+  },
+  create(params) {
+    return ApiService.post("talk", { article: params });
+  },
+  update(slug, params) {
+    return ApiService.update("talk", slug, { article: params });
+  },
+  destroy(slug) {
+    return ApiService.delete(`talk/${slug}`);
+  }
+};
+
+export const LoginService = {
+  get(slug) {
+    if (typeof slug !== "string") {
+      throw new Error(
+        "[RWV] CommentsService.get() article slug required to fetch comments"
+      );
+    }
+    return ApiService.get("articles", `${slug}/comments`);
+  },
+
+  post(slug, payload) {
+    return ApiService.post(`articles/${slug}/comments`, {
+      comment: { body: payload }
+    });
+  },
+
+  destroy(slug, commentId) {
+    return ApiService.delete(`articles/${slug}/comments/${commentId}`);
+  }
+};
