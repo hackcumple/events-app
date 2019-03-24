30646ef62f58629220c06744394b4a6db320014c
dominik.kownacki <kowdominik@wp.pl>
2019-03-24 11:17:10 +0100

    Add attendees chart

--- ing-event-client/src/views/AttendeesChart.vue
+++ ing-event-client/src/views/AttendeesChart.vue
@@ -7,24 +7,9 @@
         <div class="wrapper-normal">
             <div class="event-img-cnt"></div>
             <h1>Co nowego w Javie 12? </h1>
-            <div class="lecturer-section" style="height: 85vh;">
-                <div style="display: flex">
-                    <span>
-                        40 <br/>
-                        30 <br/>
-                        20 <br/>
-                        10 <br/>
-                    </span>
-                    <trend
-                            :data="[0, 2, 5, 9, 15, 22, 19, 18, 16, 20, 24]"
-                            auto-draw
-                            smooth>
-                    </trend>
-                </div>
-                <div style="text-align: center;">
-                    <span>Time: 5 10 15 20 25 30 35 40 45 50 55 60</span>
-                </div>
-            </div>
+            <div class="attendees-chart-title">Zależność liczby uczestników od czasu prezentacji.</div>
+            <img class="attendees-chart" src= '../assets/chart1.png'  >
+
         </div>
     </div>
 
