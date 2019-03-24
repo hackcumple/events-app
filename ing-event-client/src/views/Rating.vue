d8e11c35c7fae8a2b0c335e7828ad0b66e9cfba6
pigeon <pigeon@localhost.localdomain>
2019-03-24 12:34:50 +0100

    last commit

--- ing-event-client/src/views/Rating.vue
+++ ing-event-client/src/views/Rating.vue
@@ -3,68 +3,80 @@
     <PrelegentMenubar/>
     <div class="wrapper-normal">
       <div class="event-img-cnt"></div>
-      <h1>Ocena przebiegu prezentacji </h1>
+      <h1>Ocena przebiegu prezentacji</h1>
       <div class="rating-chart">
-
-      <bar-chart :chart-data="datacollection"></bar-chart>
-    </div>
+        <bar-chart :chart-data="datacollection"></bar-chart>
+      </div>
       <!--<div class="questions">-->
-        <!--<h2>Najczęściej powtarzane słowa: </h2>-->
+      <!--<h2>Najczęściej powtarzane słowa: </h2>-->
       <!--</div>-->
     </div>
   </div>
 
   <!--<div class="wrapper-of-centered-cnt darkbg">-->
-    <!--<PrelegentMenubar/>-->
-      <!--<div class="event-img-cnt"></div>-->
+  <!--<PrelegentMenubar/>-->
+  <!--<div class="event-img-cnt"></div>-->
 
-    <!--<div class="centered-cnt">-->
-    <!--<h1>Ocena przebiegu prezentacji</h1>-->
-    <!--<bar-chart :chart-data="datacollection"></bar-chart>-->
-    <!--</div>-->
+  <!--<div class="centered-cnt">-->
+  <!--<h1>Ocena przebiegu prezentacji</h1>-->
+  <!--<bar-chart :chart-data="datacollection"></bar-chart>-->
+  <!--</div>-->
   <!--</div>-->
 </template>
 
 <script>
-  import BarChart from '../components/LineChart.js'
-  import PrelegentMenubar from "@/components/PrelegentMenubar.vue";
+import BarChart from "../components/LineChart.js";
+import PrelegentMenubar from "@/components/PrelegentMenubar.vue";
 
-  export default {
-    components: {
-      BarChart,
-      PrelegentMenubar
-    },
-    data () {
-      return {
-        datacollection: null
-      }
-    },
-    mounted () {
-      this.fillData()
-    },
-    methods: {
-      fillData () {
-        this.datacollection = {
-          labels: ['0 - 15 min', '15 - 30 min', '30 - 45 min','45 - 60 min'],
-          datasets: [
+export default {
+  components: {
+    BarChart,
+    PrelegentMenubar
+  },
+  data() {
+    return {
+      datacollection: null
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: ["0 - 15 min", "15 - 30 min", "30 - 45 min", "45 - 60 min"],
+        datasets: [
+          {
+            label:
+              "Średnia ocen przyznanych przez uczestników w kolejnych etapach prezentacji",
+            backgroundColor: "#e66611",
+            data: [
+             2,5,3,0
+            ]
+          }
+        ],
+        scales: {
+          yAxes: [
             {
-              label: 'Średnia ocen przyznanych przez uczestników w kolejnych etapach prezentacji',
-              backgroundColor: '#e66611',
-              data: [this.getRandomInt(), this.getRandomInt(),this.getRandomInt(),this.getRandomInt()]
+              ticks: {
+                stepSize: 1,
+                maxTicksLimit: 1
+              }
             }
           ]
         }
-      },
-      getRandomInt () {
-        return Math.floor(Math.random()*5)+1;
-      }
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (5 - 1) + 1);
     }
   }
+};
 </script>
 
 <style>
-  .small {
-    max-width: 600px;
-    margin:  150px auto;
-  }
+.small {
+  max-width: 600px;
+  margin: 150px auto;
+}
 </style>
